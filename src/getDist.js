a07687b1dd044f382b303e015630a27dc616336f
Will Bohlen <wrbohlen@gmail.com>
2023-02-17 01:06:38 -0500

    I just want to go to bed

--- src/getDist.js
+++ src/getDist.js
@@ -4,10 +4,25 @@ import unmute from './unmute';
 
 // import this on your HTML
 window.addEventListener('load', () => {
+
+    // Create an audio context instance if WebAudio is supported
+    let context = (window.AudioContext || window.webkitAudioContext) ?
+    new (window.AudioContext || window.webkitAudioContext)() : null;
+    
+    // Decide on some parameters
+    let allowBackgroundPlayback = false; // default false, recommended false
+    let forceIOSBehavior = false; // default false, recommended false
+    // Pass it to unmute if the context exists... ie WebAudio is supported
+    if (context)
+    {
+    // If you need to be able to disable unmute at a later time, you can use the returned handle's dispose() method
+    // if you don't need to do that (most folks won't) then you can simply ignore the return value
+    unmute(context, allowBackgroundPlayback, forceIOSBehavior);
+    }
+
     const camera = document.querySelector('[camera]');
     const marker = document.querySelector('a-marker');
     let check;
-
 
     const synth = new Tone.Synth({
         envelope : {
@@ -51,11 +66,4 @@ window.addEventListener('load', () => {
             synth.triggerRelease();
         })
     }
-
-    
-    window.addEventListener('touchend', () => {
-        unmute(Tone.getContext());
-        unmute(synth.context);
-        synth.context.resume();
-    })
 })
