b6bd5ee1ce50cddf56d269e555a3b3eb332bc321
Will Bohlen <wrbohlen@gmail.com>
2023-02-16 23:48:14 -0500

    hey! it's functional!

--- src/getDist.js
+++ src/getDist.js
@@ -1,27 +1,50 @@
+import * as Tone from 'tone';
+
 // import this on your HTML
 window.addEventListener('load', () => {
     const camera = document.querySelector('[camera]');
     const marker = document.querySelector('a-marker');
     let check;
 
+    const synth = new Tone.Synth({
+        envelope : {
+            attack: 2,
+            decay: 0,
+            sustain: 1,
+            release: 2
+        },
+        oscillator : {
+            type: "sine"
+        },
+        volume: -20
+    }).toDestination();
+
+    synth.volume.minValue = -50;
+    synth.volume.maxValue = 0;
+
     if (marker) {
         marker.addEventListener('markerFound', () => {
             let cameraPosition = camera.object3D.position;
             let markerPosition = marker.object3D.position;
             let distance = cameraPosition.distanceTo(markerPosition)
+            console.log(markerPosition);
+
+            synth.triggerAttack("C4", Tone.now());
 
             check = setInterval(() => {
                 cameraPosition = camera.object3D.position;
                 markerPosition = marker.object3D.position;
                 distance = cameraPosition.distanceTo(markerPosition)
+                
+                synth.volume.value = distance * 3;
 
                 // do what you want with the distance:
-                console.log(distance);
-            }, 100);
+            }, 40);
         });
 
         marker.addEventListener('markerLost', () => {
-        clearInterval(check);
+            clearInterval(check);
+            synth.triggerRelease();
         })
     }
 })
